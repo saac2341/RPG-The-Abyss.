a1dfbf27c6fd9842022f46abff8b9ef62c915973
Alan <alanrodrigot1@gmail.com>
2024-10-07 14:29:05 -0600

    Merge remote-tracking branch 'origin/main-cpp' into main-cpp

--- src/rpg/items/armors/Armor.java
+++ src/rpg/items/armors/Armor.java
@@ -1,4 +1,22 @@
 package rpg.items.armors;
 
-public class Armor {
+import rpg.enums.ArmorType;
+import rpg.enums.ItemType;
+import rpg.items.Equipment;
+
+import java.util.HashMap;
+
+public abstract class Armor extends Equipment {
+
+    protected ArmorType armorType;
+
+    public Armor(String name, String description, int price, ArmorType armorType) {
+        super();
+        this.armorType = armorType;
+    }
+
+    public ArmorType getArmorType() {
+        return armorType;
+    }
+
 }
