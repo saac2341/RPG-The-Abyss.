8d41f4e4d63bcde6e4840afa381ccdc8198710e5
Alan <alanrodrigot1@gmail.com>
2024-11-12 14:56:52 -0600

    Merge remote-tracking branch 'origin/main-cpp' into main-cpp

--- src/rpg/items/miscs/Misc.java
+++ src/rpg/items/miscs/Misc.java
@@ -1,15 +1,51 @@
 package rpg.items.miscs;
+
 import rpg.items.Item;
+
+import java.io.Serializable;
 
 /**
  * Creation de demás objetos.
  */
-public abstract class Misc extends Item {
-    public Misc(String name){
-        super(name);
+public abstract class Misc extends Item implements Serializable {
+
+    protected boolean consumable;
+
+    protected boolean stackable;
+    protected int quantity;
+    protected int maxQuantity;
+
+    public Misc(){
+        super();
+        maxQuantity = 99;
     }
 
-    public String toString(){
-        return "Objeto    "+name+"  Description   "+description+"    puntos "+ price;
+    public abstract void use ();
+
+    public boolean isConsumable() {
+        return consumable;
     }
+
+    public boolean isStackable(){
+        return stackable;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void increaseQuantity(int amount) {
+        quantity += amount;
+        if (quantity > maxQuantity){
+            quantity = maxQuantity;
+        }
+    }
+
+    public void decreaseQuantity(int amount) {
+        quantity -= amount;
+        if (quantity < 0) {
+            quantity = 0;
+        }
+    }
+
 }
